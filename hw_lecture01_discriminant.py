f1a95828e11bf36e3f05ea4a4b97ec325a2208ed
anastasia tsiguleva <asy.tsiguleva@gmail.com>
2024-10-07 01:05:40 +0700

    homework discriminant

---
+++ hw_lecture01_discriminant.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Oct  6 21:49:18 2024
+
+@author: Компьютер
+"""
+
+print ("Введите коэффициент a: ")
+a = input()
+print ("Введите коэффициент b: ")
+b = input()
+print ("Введите коэффициент с: ")
+c = input()
+
+a = float(a)
+b = float(b)
+c = float(c)
+
+D = b**2 - 4 * a * c
+
+print ("Дискриминант равен: ", D)
